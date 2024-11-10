6a6575ac479c328204279952e1e08208cdaf337d
4udofruct1k <4udofructik@gmail.com>
2024-11-10 22:09:30 +0500

    Update homework4.py

--- homework4/homework4.py
+++ homework4/homework4.py
@@ -1,5 +1,21 @@
-a = int(input("Введите a: "))
-b = int(input("Введите b: "))
-c = int(input("Введите c: "))
-x = a/b - 2*c
-print("Результат уравнения X = a/b - 2*c =", x)
+while True:
+    a = input("Введите целое a: ")
+    if a.isdigit():
+        break
+    else:
+        print("Неверный ввод")
+while True:
+    b = input("Введите целое b: ")
+    if b.isdigit():
+        break
+    else:
+        print("Неверный ввод")
+while True:
+    c = input("Введите целое c: ")
+    if c.isdigit():
+        break
+    else:
+        print("Неверный ввод")
+if b != 0:
+    x = int(a)/int(b) - 2*int(c)
+    print("Результат уравнения X = a/b - 2*c =", x)
